12252998b2fe18170f3021d8f77858bc8d00df90
benybodipo <benybodipo@gmail.com>
2022-03-14 16:49:20 +0200

    Made navbar responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,12 +10,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarColor01">
               <ul class="navbar-nav ms-auto flex-nowrap">
-                <li class="nav-item notification pr-3">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    <fa :icon="['far', 'bell']" class="bell"/>
-                  </a>
-                </li>
-                <li class="nav-item profile">
+                <li class="nav-item profile d-none d-xs-block d-sm-block d-block d-lg-none">
                   <a class="nav-link clearfix profile-link pl-2" href="#">
                     <img :src="profileImg" alt="">
                     <div class="profile-info">
@@ -24,8 +19,25 @@
                     </div>
                   </a>
                 </li>
-                <li class="nav-item profile d-none d-sm-block " v-if="accounts">
-                  <router-link class="nav-link" :to="{path: ('/billing/'+ accounts[0].account_id)}" aria-current="true">
+                <li class="nav-item notification pr-3">
+                  <a class="nav-link active" aria-current="page" href="#">
+                    <span class="bell-container">
+                      <fa :icon="['far', 'bell']" class="bell"/>
+                    </span>
+                    <span class="d-none d-xs-block d-sm-block d-lg-none" >Notifications</span>
+                  </a>
+                </li>
+                <li class="nav-item profile d-xs-none d-sm-none d-md-none d-lg-block">
+                  <a class="nav-link clearfix profile-link pl-2" href="#">
+                    <img :src="profileImg" alt="">
+                    <div class="profile-info">
+                      <h5 class="m-0 name">First Lastname</h5>
+                      <small class="email">firstlastname@bonline.com</small>
+                    </div>
+                  </a>
+                </li>
+                <li class="nav-item profile d-none d-sm-block d-lg-none" v-if="accounts">
+                  <router-link class="nav-link" :to="{path: ('/billing/'+ accounts[0].account_id)}" style="color:inherit;">
                     <fa :icon="['fas', 'wallet']" />
                     Billing
                   </router-link>
